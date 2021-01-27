b5cecc70ca5e5950c5e07e0f57400a6050db8853
Dragonizedpizza <70718540+Dragonizedpizza@users.noreply.github.com>
2021-01-27 16:00:30 +0530

    Update ping.js

--- bot-files/commands/information/ping.js
+++ bot-files/commands/information/ping.js
@@ -6,9 +6,12 @@ module.exports = {
     description: "Returns the bot's ping!",
     run: async(client, message) => {
         const embed = new Discord.MessageEmbed()
-        .setAuthor("<:DragonNight_Settings:803919154405965864> DragonNight Latency")
-        .setDescription(`Client Latency - \`\`${client.ws.ping}\ms`)
+        .setAuthor("DragonNight Latency", "https://media.discordapp.net/attachments/803204453321670700/803930305135116288/circle-cropped_13.png")
+        .addField("Latency", `\`${msg.createdTimestamp - timestamp}ms\``)
+        .addField("API Latency", `\`${client.ws.ping}\``)
+        .addField("Description", `**Latency** is the amount of time the bot took to send the message. \n ** API latency is the ping to the Discord API.`)
         .setColor('#034ea2')
+        .setImage("https://media.discordapp.net/attachments/803204453321670700/803931313483939850/Ping.png?width=1025&height=342")
         message.channel.send(embed)
     }
 }
