a6dbf82dbe4426e0adedbf4c51228f83ba51b65e
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-01-28 21:55:15 +1100

    Updated the new invite command.
    Added poggers images and sick details.

--- bot-files/commands/information/invite.js
+++ bot-files/commands/information/invite.js
@@ -1,22 +1,17 @@
-const config = require('../../../config.json'); 
+const Discord = require("discord.js");
+
 module.exports = {
-    name: "invite",
-    aliases: ["inviteme", "botinvite"],
-    description: "Sends the bot invite, along with a few other invites.",
-    run: async(client, message, args) => {
-        message.channel.send({ embed: {
-            title: "Invite Me!",
-            fields: [
-                {
-                  name: "Bot Invite!",
-                  value: "You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=803169312827113483&permissions=8&scope=bot) or by using the following link! \n Link: https://discord.com/oauth2/authorize?client_id=803169312827113483&permissions=8&scope=bot"
-                },
-                {
-                    name: "Support Server!",
-                    value: "You can join the support server by clicking [here](https://discord.gg/VQ6cU2cZwV) or using the following link! \n Link: https://discord.gg/VQ6cU2cZwV"
-                }
-              ],
-            footer: {text: `Tip: Use the '${config.prefix}vote' command to vote for us!`}
-            }})
-    }
+  name: "invite",
+  aliases: ["inv"],
+  description: "Invite The Bot!",
+  run: async (client, message, args) => {
+
+    const embed = new Discord.MessageEmbed()
+    .setAuthor("Invite DragonNight")
+    .addField("<:DragonNight_Plus:804245667522019348>  Invite Link", "Click [here](https://discord.com/oauth2/authorize?client_id=803169312827113483&permissions=8&scope=bot) to invite the bot")
+    .addField("<:DragonNight_Info:803920668080472144>  Support Server Link", "Click [here](https://discord.gg/dNc3EvABCA) to join the support server.")
+    .setColor("#90ee90")
+    .setImage("https://media.discordapp.net/attachments/770953232318726144/804251677950869514/Untitled_9.jpg?width=1025&height=342")
+    message.channel.send(embed)
+  }
 }
