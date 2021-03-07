ab8cf68e0f3d477b08583364351b820ecb91af2a
dragonizedpizza <dragonizedpizza@dragonizedpizza.dragonizedpizza>
2021-03-07 23:23:44 +0100

    music

--- bot/events/music/trackStart.js
+++ bot/events/music/trackStart.js
@@ -3,5 +3,9 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'trackStart',
 	run: async (message, track) => {
-    message.channel.send(new MessageEmbed().setAuthor(`Now Playing ${track.title}`, "https://raw.githubusercontent.com/HurricanoBot/HurricanoImages/master/SetAuthorEmojis/Success.png").setDescription(`Now playing ${track.title} in ${message.member.voice.channel.name}.`).setURL(track.url).setThumbnail(track.thumbnail));
+    message.channel.send(new MessageEmbed().setAuthor(`Now Playing ${track.title}`, "https://raw.githubusercontent.com/HurricanoBot/HurricanoImages/master/SetAuthorEmojis/Success.png")
+    .setDescription(`Now playing ${track.title} in ${message.member.voice.channel.name}.`)
+    .addField('Additional Information', `Duration: ${track.duration} \n URL: [here](${track.url}) \n Views: ${track.views} \n Author: ${track.author}`)
+    .setFooter(`Requested by ${track.requestedBy.username}`, track.requestedBy.displayAvatarURL)
+    .setThumbnail(track.thumbnail));
 }};
