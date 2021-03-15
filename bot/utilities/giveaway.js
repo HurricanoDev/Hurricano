2621a1c32206553698d9c932e00915eb9ac42ef9
Dragonizedpizza <70718540+Dragonizedpizza@users.noreply.github.com>
2021-03-15 22:40:00 +0100

    wait

--- bot/utilities/giveaway.js
+++ bot/utilities/giveaway.js
@@ -2,7 +2,6 @@ const database = require("../schemas/giveaway");
 
 const { GiveawaysManager } = require("discord-giveaways");
 module.exports = class Gmanager extends GiveawaysManager {
-<<<<<<< HEAD
   async getAllGiveaways() {
     return await database.find({});
   }
@@ -27,28 +26,3 @@ module.exports = class Gmanager extends GiveawaysManager {
     return true;
   }
 };
-=======
-      async getAllGiveaways() {
-          return await database.find({});
-      }
-      async refreshStorage() {
-          return global.client.shard.broadcastEval(() => this.giveawaysManager.getAllGiveaways());
-    }
-      async saveGiveaway(messageID, giveawayData) {
-          await database.create(giveawayData);
-          return true;
-      }
-      async editGiveaway(messageID, giveawayData) {
-          await database
-              .findOneAndUpdate({ messageID: messageID }, giveawayData)
-              .exec();
-          return true;
-      }
-      async deleteGiveaway(messageID) {
-          await database
-              .findOneAndDelete({ messageID: messageID })
-              .exec();
-          return true;
-   }
-}
->>>>>>> ff32359562338ea542d857f3b6d2c638e3efdc1c
