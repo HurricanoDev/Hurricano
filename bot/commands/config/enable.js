cc996b1c393a1f901a712942d683dd6de5c5253c
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-04-10 10:36:36 +0200

    Update enable.js

--- bot/commands/config/enable.js
+++ bot/commands/config/enable.js
@@ -34,7 +34,7 @@ module.exports = class EnableCommand extends Command {
       let guildSchema = await client.schemas.guild.findOne({
         id: message.guild.id,
       });
-      let disabledModules = guildSchema.disabledModules;
+      let disabledModules = await guildSchema.disabledModules;
       if (!disabledModules.includes(argss)) {
         message.channel.sendError(
           message,
