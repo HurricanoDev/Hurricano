5869f7b098abfbf502bab8e3b7513316fdb5a7bc
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-04-29 22:16:27 +0200

    ee

--- bot/utilities/config.js
+++ bot/utilities/config.js
@@ -1,5 +1,4 @@
 let config = require(`${process.cwd()}/config.json`);
-require('dotenv').config();
 module.exports = {
   token: config.token || process.env.token,
   prefix: config.prefix || process.env.prefix,
