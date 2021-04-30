b392b152e01bfc726f8d905bd57b94f67ab34b10
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-04-30 16:49:31 +1000

    Add files via upload
    eeee ooo u aaaa aaaa

---
+++ bot/commands/image/burn.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const Command = require("@Command");
+module.exports = new Command({
+  name: "burn",
+  cooldown: 5,
+  description: "Burn someone's avatar :(",
+  async run(message, args) {
+    const canvacord = require('canvacord')
+    let person = message.mentions.users.first() || message.author;
+    let avatar = person.displayAvatarURL({ dynamic: false, format: 'png' });
+    let img = await canvacord.Canvas.burn(avatar, 4)
+    let attachment = new Discord.MessageAttachment(img, "burn.png");
+    message.channel.send(attachment)
+  },
+});
