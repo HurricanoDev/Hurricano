a28e9ea9fccb56a06ef095a551a45086895ff109
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-04-30 17:52:05 +1000

    Update supreme.js

--- bot/commands/image/supreme.js
+++ bot/commands/image/supreme.js
@@ -7,8 +7,6 @@ module.exports = new Command({
   description: "Convert text to supreme text :D",
   slash: false,
   async run(message, args, quicksend) {
-    const massage = args.join(" ");
-    if (!massage) return message.reply(embed1);
     let hasil = `https://api.alexflipnote.dev/supreme?text=${massage}`;
     let Attach = new Discord.MessageAttachment(hasil, "supreme.png");
     message.reply(Attach);
