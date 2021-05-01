e2bffbabea9e59e587416f94d986692d68a82990
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-01 10:30:50 +1000

    Create ghostping.js

---
+++ bot/commands/config/ghostping.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const Command = require("@Command");
+const ghostPingSchema = require("../../schemas/ghostping.js");
+
+module.exports = new Command({
+  name: "ghostping",
+  aliases: ["gp", "setghostpingchannel"],
+  userPermissions: ["ADMINISTRATOR"],
+  args: "Please tag a channel!",
+  cooldown: 20,
+  description: "Set your server's custom prefix!",
+  async run(message, args, quicksend) {
+    onst { mentions, guild } = message;
+
+    const targetChannel = mentions.channels.first();
+
+    await ghostPingSchema.findOneAndUpdate(
+      {
+        _id: guild.id,
+      },
+      {
+        _id: guild.id,
+        channelId: targetChannel.id,
+      },
+      {
+        upsert: true,
+      }
+    );
+
+    message.sendSuccessReply("Success!", `The ghost ping channel has been set to <#${targetChannel.id}>`);
+  },
+});
