3a61010379365d46324114eb74dde9749c091f11
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-01 10:39:23 +1000

    Update messageDelete.js
    ghostping detector

--- bot/events/bot/messageDelete.js
+++ bot/events/bot/messageDelete.js
@@ -1,3 +1,7 @@
+const ghostPingSchema = require("../models/ghost-ping-schema.js");
+const { MessageEmbed } = require("discord.js");
+const cache = {};
+
 module.exports = {
   name: "messageDelete",
   run: async (message, client) => {
@@ -10,5 +14,34 @@ module.exports = {
         ? message.attachments.first().proxyURL
         : null,
     });
+    //GhostPing Detector Start-
+    const { content, channel, author, guild, mentions } = message;
+
+    if (!author || author.bot || mentions.users.size === 0) {
+      return;
+    }
+
+    let channelId = cache[guild.id];
+    if (!channelId) {
+      const result = await ghostPingSchema.findById(guild.id);
+      if (!result) {
+        return;
+      }
+
+      channelId = result.channelId;
+      cache[guild.id] = channelId;
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle("Possible Ghost Ping Detected")
+      .setDescription(`Message\n\n"${content}"`)
+      .addField("Channel", channel)
+      .addField("Message Author", author)
+      .setColor("#FFFFFF")
+
+    const targetChannel = guild.channels.cache.get(channelId);
+    if (targetChannel) {
+      targetChannel.send(embed);
+    }
   },
 };
