6c5a655cc7d5f6669f6b34e2736db9f2af1b67ff
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-03 11:51:24 +1000

    Update msglogs.js

--- bot/commands/config/msglogs.js
+++ bot/commands/config/msglogs.js
@@ -5,7 +5,7 @@ module.exports = new Command({
   aliases: ["messagelogs", "messagelog"],
   userPermissions: ["ADMINISTRATOR"],
   cooldown: 20,
-  description: "Set your server's message logs.",
+  description: "Set your server's message logs. This includes: Message deletion, Ghostping logging. More coming soon!",
   async run(message, args) {
     const prefix = await client.db.guild.getPrefix(message.guild.id);
     if (!args.length)
