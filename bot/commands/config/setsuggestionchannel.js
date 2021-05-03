001a950ec3c250575d6e2000d232a73a8add677c
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-03 21:01:46 +1000

    Update setsuggestionchannel.js

--- bot/commands/config/setsuggestionchannel.js
+++ bot/commands/config/setsuggestionchannel.js
@@ -29,7 +29,7 @@ module.exports = new Command({
       }
     )
 
-    message.sendSuccessReply("Success!", `The suggestions channel has been set to ${channel}`)
+    message.channel.sendSuccessReply("Success!", `The suggestions channel has been set to ${channel}`)
 
     fetchSuggestionChannels(guildId)
   },
