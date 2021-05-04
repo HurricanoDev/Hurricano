2885aac29908744cc8b818c9f739066ea09853c8
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-04 17:10:29 +1000

    Update guildCreate.js

--- bot/events/bot/guildCreate.js
+++ bot/events/bot/guildCreate.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "guildCreate",
   run: async (guild, client) => {
@@ -13,5 +14,15 @@ module.exports = {
     } catch (err) {
       client.logger.warn(err);
     }
+    const progressChannel = client.channels.cache.get("839034066795364353");
+    if (!progressChannel) console.log("No progress channel found!");
+    const guildEmbed = new MessageEmbed()
+      .setTitle("New Guild!")
+      .setImage(guild.iconURL)
+      .addField("Name:", `${guild.name}`)
+      .addField("Owner:", `${guild.owner}`)
+      .addField("Server Count", `${client.guilds.cache.size} servers.`)
+      .setColor("#6082b6");
+    progressChannel.send(guildEmbed);
   },
 };
