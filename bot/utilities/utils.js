0fc0c952d7569fd5559de3d0844f31425f0564d0
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-06 12:11:18 +1000

    Create utils.js

---
+++ bot/utilities/utils.js
@@ -0,0 +1,19 @@
+
+const { MessageEmbed } = require('discord.js');
+const schedule = require('node-schedule');
+const { stripIndent } = require('common-tags');
+
+function trimStringFromArray(arr, maxLen = 2048, joinChar = '\n') {
+  let string = arr.join(joinChar);
+  const diff = maxLen - 15; // Leave room for "And ___ more..."
+  if (string.length > maxLen) {
+    string = string.slice(0, string.length - (string.length - diff)); 
+    string = string.slice(0, string.lastIndexOf(joinChar));
+    string = string + `\nAnd **${arr.length - string.split('\n').length}** more...`;
+  }
+  return string;
+}
+
+module.exports = {
+  trimStringFromArray,
+};
