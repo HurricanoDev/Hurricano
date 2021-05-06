a89eef32edcf18954d8d934c2b5d85acca5162b6
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-06 12:18:58 +1000

    Update systemmessage.js

--- bot/commands/owner/systemmessage.js
+++ bot/commands/owner/systemmessage.js
@@ -9,6 +9,7 @@ module.exports = new Command({
   async run(client, message, args) {
     const Schema = client.schemas.guild;
     const msg = args.join(" ");
+    const guilds = []
     client.guilds.cache.forEach(guild => {
       const data = await Schema.findOne({ id: guild.id });
       const systemChannel = guild.channels.cache.get(data.systemChannel);
@@ -22,9 +23,20 @@ module.exports = new Command({
           .setThumbnail(client.user.displayAvatarURL())
           .setDescription(msg)
           .setTimestamp()
-          .setColor(message.guild.me.displayHexColor);
+          .setColor("#6082b6");
         systemChannel.send(embed);
-      } else continue;
+      } else guilds.push(guild.name);
     });
+    if (guilds.length > 0) {
+      const description = client.utils.trimStringFromArray(guilds);
+
+      const embed = new MessageEmbed()
+        .setTitle('System Message Failures')
+        .setDescription(description)
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor("#6082b6");
+      message.channel.send(embed);
+    }
   }
 }) 
