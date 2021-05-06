246855f2f3219ee5cdcf81c69bea4bfcbb715faa
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-06 12:06:40 +1000

    Update systemmessage.js
    oops, forgot to add ownerOnly option

--- bot/commands/owner/systemmessage.js
+++ bot/commands/owner/systemmessage.js
@@ -5,6 +5,7 @@ module.exports = new Command({
   name: "systemmessage",
   description: "Send a message to all servers with a system channel.",
   args: "Give me a message to send to the servers with a system channel.",
+  ownerOnly: true,
   async run(client, message, args) {
     const Schema = client.schemas.guild;
     const msg = args.join(" ");
