a84f2e8a13fad292ee31ab43d5f514ff5e0024ba
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-09 20:59:19 +1000

    Create settings.js

---
+++ bot/commands/config/settings.js
@@ -0,0 +1,45 @@
+const Command = require("@Command");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = new Command({
+  name: "settings",
+  cooldown: 5,
+  userPermissions: ["ADMINISTRATOR"],
+  description: "View the server's configuration.",
+  async run(message, args) {
+    const guildInfo = await client.schemas.guild.findOne({
+      id: message.guild.id,
+    });
+    const prefix = await client.db.guild.getPrefix(message.guild.id);
+    //System Settings
+    let systemChannel =
+      message.guild.channels.cache.get(guildInfo.systemChannel) || "None";
+    let autoRole = message.guild.roles.cache.get(guildInfo.autoRole) || "None";
+    let suggestionChannel =
+      message.guild.channels.cache.get(guildInfo.suggestionChannel) || "None";
+
+    const mainEmbed = new MessageEmbed()
+      .setTitle("Settings")
+      .setDescription(`**More information:** \`${prefix}settings [category]\``)
+      .addField("System", "`4` settings")
+      .addField("Logging", "`2` settings")
+      .setTimestamp()
+      .setColor("#6082b6")
+      .setFooter(`© Hurricano ${client.version}`);
+
+    switch (args[0]) {
+      case "system":
+        const systemEmbed = new MessageEmbed()
+          .setTitle("Settings: **`System`**")
+          .setColor("#6082b6")
+          .addField("Prefix", `\`${prefix}\``)
+          .addField("System Channel", `\`${systemChannel}\``)
+          .addField("Auto Role", `\`${autoRole}\``)
+          .addField("Suggestion Channel", `\`${suggestionChannel}\``)
+          .setTimestamp()
+          .setFooter(`Hurricano ${client.version}`);
+        await message.channel.send(systemEmbed);
+        break;
+    }
+  },
+});
