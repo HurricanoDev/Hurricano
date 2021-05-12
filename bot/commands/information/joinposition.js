01be5134c810300cee73be249ef4482a71bbf992
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-12 18:59:34 +1000

    Update joinposition.js

--- bot/commands/information/joinposition.js
+++ bot/commands/information/joinposition.js
@@ -8,17 +8,32 @@ module.exports = new Command({
   async run(message, args) {
     const member = await client.functions.getMember(true, message, args[0]);
     const members = (await message.guild.members.fetch())
-      .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp).array();
+      .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
+      .array();
 
-      const position = new Promise((ful) => {
-        for (let i = 1; i < members.length + 1; i++) {
-          if (members[i - 1].id === member.id) ful(i);
-        }
-      });
+    const position = new Promise((ful) => {
+      for (let i = 1; i < members.length + 1; i++) {
+        if (members[i - 1].id === member.id) ful(i);
+      }
+    });
+    function ordinal(i) {
+      var j = i % 10,
+        k = i % 100;
+      if (j == 1 && k != 11) {
+        return i + "st";
+      }
+      if (j == 2 && k != 12) {
+        return i + "nd";
+      }
+      if (j == 3 && k != 13) {
+        return i + "rd";
+      }
+      return i + "th";
+    }
 
     await message.sendSuccessReply(
       "Success!",
-      `${member} is position ${(await position)} when joining the server!`
+      `${member} is the ${ordinal(await position)} member to join the server!`
     );
   },
 });
