995d6d07da3d41e7c9f1b9c551164360521585a5
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-05-12 19:21:14 +1000

    Update Client.js

--- bot/Client.js
+++ bot/Client.js
@@ -175,7 +175,7 @@ class Client extends Discord.Client {
         if (user) return user;
         if (!user) return null;
       },
-      getOrdialSuffix: async (i) => {
+      getOrdinalSuffix: async (i) => {
         var j = i % 10,
           k = i % 100;
         if (j == 1 && k != 11) {
