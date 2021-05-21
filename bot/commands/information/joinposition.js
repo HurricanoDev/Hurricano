9e958e1e020969188185cf7b2556108ba91c0a20
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-05-21 13:15:11 +0200

    JoinPosition fixes :D

--- bot/commands/information/joinposition.js
+++ bot/commands/information/joinposition.js
@@ -5,12 +5,12 @@ module.exports = new Command({
   description: "View the position a user joined the server.",
   async run(message, args) {
     let member = parseInt(args[0]);
-    if (!member)
+    if (!member || member.length > 16)
     member = await client.functions.getMember(true, message, args[0]);
     const members = (await message.guild.members.fetch())
       .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
       .array();
-
+      if (parseInt(args[0]) && member < 0 || parseInt(args[0]) && member > message.guild.memberCount) return message.channel.sendError(message, 'Invalid Number!', 'Please provide a valid number!');
     let position;
     if (!parseInt(member)) {
     for (let i = 1; i < members.length + 1; i++) {
