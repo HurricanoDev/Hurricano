b3a88dc89a63cd5486ae432f662fe2597c77dc3d
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-05-21 10:24:17 +0200

    ehehe

--- bot/commands/information/joinposition.js
+++ bot/commands/information/joinposition.js
@@ -4,21 +4,26 @@ module.exports = new Command({
   aliases: ["joinp"],
   description: "View the position a user joined the server.",
   async run(message, args) {
-    const member = await client.functions.getMember(true, message, args[0]);
+    const member = parseInt(args[0]);
+    if (!number)
+    member = await client.functions.getMember(true, message, args[0]);
     const members = (await message.guild.members.fetch())
       .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp)
       .array();
 
     let position;
+    if (!parseInt(member)) {
     for (let i = 1; i < members.length + 1; i++) {
       if (members[i - 1].id === member.id) position = i;
     }
+  } else {
+    position = members[member];
+  }
     await message.sendSuccessReply(
       "Success!",
       `${member} is the ${client.functions.getOrdinalSuffix(
         position
       )} member to join the server!`
     );
-    console.log("cee");
   },
 });
