c0c8b702cc0c5af6fd7f17c4ab93b85327f8e510
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-05-30 14:18:42 +0200

    Website fixes.

--- website/index.js
+++ website/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.set("views", "./views")
+app.set("views", "./website/views")
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -15,4 +15,3 @@ app.listen(client.config.website.port, client.config.website.ip, () => {
   console.log("Server Up!");
 });
 
-// Run npm i express ejs to install
