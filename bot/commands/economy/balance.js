f8099558211bfc7c1c2141ef4ad434b4c4391750
Militia21 <>
2021-06-08 11:27:03 +0000

    few fixes

--- bot/commands/economy/balance.js
+++ bot/commands/economy/balance.js
@@ -8,7 +8,7 @@ module.exports = new Command({
   description: "Check how many coins you have.",
   async run(message, args) {
     const target = client.functions.getMember(true, message, args[0]);
-    const userData = client.db.users.cache.get(target.user.id);
+    const userData = client.db.users.cache.get(target.id);
     
     const wallet = userData.wallet;
     const bank = userData.bank;
