9aecce693f3a90a6f8929da735b5bbf3c6ff7d7a
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-06-11 02:32:19 +0000

    new function

--- bot/Client.js
+++ bot/Client.js
@@ -221,6 +221,22 @@ class Client extends Discord.Client {
         }
         return i + "th";
       },
+      randomInArray: (array) => {
+        var currentIndex = array.length,
+          randomIndex;
+
+        while (0 !== currentIndex) {
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+          ];
+        }
+
+        return array;
+      },
       createOptionsEmbed: (
         name,
         syntax,
