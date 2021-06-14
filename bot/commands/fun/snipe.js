b3940fd90a95a26cc5656a1c82f0d8fd01d54951
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-06-14 08:10:39 +0200

    fix

--- bot/commands/fun/snipe.js
+++ bot/commands/fun/snipe.js
@@ -5,7 +5,7 @@ module.exports = new Command({
   description: "Snipe a deleted message.",
   ownerOnly: true,
   async run(message, args) {
-    function snipeEmbed(messageSnipe) {
+    function snipeEmbed(messageSnipe, number, arrayLength) {
       const embed = new MessageEmbed()
       .setAuthor("Sniped!", messageSnipe.author.displayAvatarURL())
       .setTitle("Content:")
@@ -13,13 +13,14 @@ module.exports = new Command({
       messageSnipe.action ? embed.addField("Action:", `**\`${messageSnipe.action}.\`**`, true) : null;
       embed.addField("Action By:", `${messageSnipe.author}, \n ID: ${messageSnipe.author.id}`, true)
       messageSnipe.attachments ? embed.addField("Attachments:", messageSnipe.attachments.join(", \n"), true) : null;
+      embed.setFooter(`Message ${number}/${arrayLength}`)
       return embed;
     }
     if (!args.length) {
       const array = client.snipes.recent.get(message.channel.id).reverse();
       if (!array) return message.channel.sendError(message, "Nothing to Snipe!", `There's nothing to snipe, ${message.author}!`);
       const messageSnipe = array[0];
-      const embed = snipeEmbed(messageSnipe);
+      const embed = snipeEmbed(messageSnipe, 1, array.length);
       message.channel.send(embed);
     }
     const number = +args[0];
@@ -28,7 +29,7 @@ module.exports = new Command({
       if (!array) return message.channel.sendError(message, "Nothing to Snipe!", `There's nothing to snipe, ${message.author}!`);
       if (array?.length < number) return message.channel.sendError(message, "Invalid Number!", `There's only \`${array.length}\` messages I can snipe!`);
       const messageSnipe = array[number - 1];
-      const embed = snipeEmbed(messageSnipe);
+      const embed = snipeEmbed(messageSnipe, number, array.length);
       message.channel.send(embed);
     }
     const num = +args[1] - 1;
@@ -38,14 +39,14 @@ module.exports = new Command({
     if (!msgs) return message.channel.sendError(message, "Nothing to Snipe!", `There's nothing to snipe, ${message.author}!`);
     if (msgs?.length < num) return message.channel.sendError(message, "Invalid Number!", `There's only \`${array.length}\` messages I can snipe!`);
     const msg = args[1] ? msgs[num] : msgs[0];
-      const embed = snipeEmbed(msg);
+      const embed = snipeEmbed(msg, num, array.length);
     return message.channel.send(embed);
   break;
   case "edit":
     const msgsE = this.client.snipes.deleted.get(message.channel.id).reverse();;
     if (msgsE?.length < num) return message.channel.sendError(message, "Invalid Number!", `There's only \`${array.length}\` messages I can snipe!`);
     const msgE = args[1] ? msgsE[num] : msgsE[0];
-    const embedE = snipeEmbed(msgE);
+    const embedE = snipeEmbed(msgE, num, array.length);
     return message.channel.send(embedE);
   }}
 });
