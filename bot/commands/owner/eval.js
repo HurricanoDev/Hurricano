b9589bde4f2948b69eb7d564af4fd31579a2bc33
dragonizedpizza <dragon@dragonizedpizza.com>
2021-06-18 04:47:17 +0200

    fix

--- bot/commands/owner/eval.js
+++ bot/commands/owner/eval.js
@@ -1,15 +1,15 @@
 const Discord = require("discord.js");
-const sourcebin = require("sourcebin");
+const sourcebin = require("../../utilities/SourcebinPost.js");
 const config = require("@config");
 const Command = require("@Command");
-const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { MessageEmbed, MessageButton } = require("discord.js");
 module.exports = new Command({
   name: "eval",
   description: "Evaluates arbituary JavaScript.",
   ownerOnly: true,
   args: "Please provide what you would like to eval!",
   async run(message, args) {
-    async function sendEmbed(content, input) {
+    async function sendEmbed(content, input, typeofOut) {
       const toEval = input;
       let embed = new MessageEmbed()
         .setTitle("Eval Output.")
@@ -18,32 +18,33 @@ module.exports = new Command({
           message.author.displayAvatarURL()
         )
         .addField(
-          "Input",
+          "Input:",
           `\`\`\`js\n${
             toEval.length > 1016
               ? "Input is larger than 1016 characters."
               : toEval
           }\`\`\``,
           true
-        );
+        )
+        .addField("Type of Output:", `\`\`\`xl\n${typeofOut}\`\`\``);
       if (content.length > 2032) {
-        const src = await sourcebin
-          .create(
+        const src = await sourcebin(
             [
               {
+                name: `Code by ${message.author.tag}`,
                 content: content,
-                language: "javascript",
-              },
-            ],
+                languageId: "js",
+              }],
             {
-              name: `Code by ${message.author.tag}`,
+              title: `Code by ${message.author.tag}`,
               description: `Output of the eval command used by ${message.author.tag}.`,
             }
-          )
+            )
           .catch((e) => {
             return e;
           });
-        const msg = await message.author.send(
+        const msg = await message.author.sendSuccess(
+          message,
           "Eval Output.",
           `The output for the eval command was larger than 2032 characters. To check it, click [here](${src.url}) or use the link: ${src.url}.`
         );
@@ -80,6 +81,7 @@ module.exports = new Command({
         : (evaled = await eval(`(async () => {
         ${code}
       })()`));
+      const type = typeof evaled;
       if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled, { depth: 4 });
       if (evaled.includes(config.token) || evaled.includes(config.mongouri))
@@ -89,7 +91,7 @@ module.exports = new Command({
           "This eval has the bot credentials! Please try without using the bot's credentials."
         );
 
-      const embed = await sendEmbed(evaled, code);
+      const embed = await sendEmbed(evaled, code, type);
       const row = new MessageButton()
         .setCustomID("toDelete")
         .setLabel("Delete?")
@@ -113,7 +115,8 @@ module.exports = new Command({
         msg.delete();
       }
     } catch (err) {
-      const embed = await sendEmbed(err, code);
+      const type = typeof err;
+      const embed = await sendEmbed(err, code, type);
       const row = new MessageButton()
         .setCustomID("toDelete")
         .setLabel("Delete?")
