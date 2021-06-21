40fc0cf25e37ed5acf0eec8e77749f5180ec7652
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-06-21 08:50:43 +0200

    Update Clyde.js

--- bot/commands/image/Clyde.js
+++ bot/commands/image/Clyde.js
@@ -1,29 +1,40 @@
+
 const Discord = require("discord.js"),
-  { MessageAttachment } = require("discord.js");
-const Command = require("@Command");
-const fetch = require('node-fetch');
+  { MessageAttachment, MessageEmbed } = require("discord.js"),
+  Command = require("@Command"),
+  fetch = require("node-fetch");
+
 module.exports = new Command({
   name: "clyde",
   cooldown: 5,
   description: "Sends a clyde message",
   async run(message, args) {
-    const text = args.slice().join(' ');
+    const text = args.slice().join(" ");
     if (!text) {
-      return message.channel.send(
-        '❎ Please provide valid text.',
+      return message.channel.sendError(
+        message,
+        "An Error Occured.",
+        "Please provide valid text."
       );
     }
 
-    const url = `https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`;
+    const url = `https://nekobot.xyz/api/imagegen?type=clyde&text=${EncodeURIComponent(
+      text
+    )}`;
 
     let response;
     try {
-      response = await fetch(url).then(res => res.json());
+      response = await fetch(url).then((res) => res.json());
+    } catch (e) {
+      return message.channel.sendError(message, "An Error Occured.", `Error: \`\`\`js\n${e.stack}\`\`\``);
     }
-    catch (e) {
-      return message.channel.send('❎ An error occured, please try again!');
-    }
-    const attachment = new MessageAttachment(response.message, 'clyde.png');
-    return message.channel.send(attachment);
+    const embed = new MessageEmbed()
+      .setAuthor("Clyde do be talkin' tho-")
+      .setDescription("Clyde do be talkin' real smooth-")
+      .setImage("attachment://clyde.png");
+    return message.channel.send({
+      embeds: [embed],
+      files: [new MessageAttachment(response.message, "clyde.png")],
+    });
   },
 });
