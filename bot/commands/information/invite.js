3c4e4e20fe39ab6d747303097a9a8e51c8af8ef8
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-06-23 00:14:22 +0000

    voting link

--- bot/commands/information/invite.js
+++ bot/commands/information/invite.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 const emojis = require("../../utilities/emojis.json");
 const Command = require("@Command");
 module.exports = new Command({
-  name: "invite",
-  aliases: ["inv"],
-  description: "Invite The Bot!",
+  name: "links",
+  aliases: ["invite", "inv", "vote"],
+  description: "Important links!",
   async run(message) {
     const embed = new Discord.MessageEmbed()
-      .setAuthor("Invite Hurricano™")
+      .setAuthor("Hurricano™ Links")
       .addField(
         emojis.signs.plus + "  Invite Link",
         "Click [here](https://discord.com/oauth2/authorize?client_id=803169312827113483&scope=bot&permissions=8589934591&scope=applications.commands%20bot) to invite the bot."
@@ -15,6 +15,10 @@ module.exports = new Command({
       .addField(
         emojis.signs.info + "  Support Server Link",
         "Click [here](https://discord.gg/dNc3EvABCA) to join the support server."
+      )
+      .addField(
+        emojis.signs.topgg + "  Top.gg Vote Link",
+        `Click [here](https://top.gg/bot/803169312827113483/vote) to vote for ${client.user.username}`
       )
       .setColor("#90ee90")
       .setImage(
