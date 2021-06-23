56059fef7466a0ebfa055e3727614f7b0d43c5c6
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-06-23 08:14:41 +0000

    pog

--- bot/Client.js
+++ bot/Client.js
@@ -265,6 +265,18 @@ class HurricanoClient extends Discord.Client {
       hasNumber: (number) => {
         return /\d/.test(number);
       },
+      shuffle: (content) => {
+        var a = content.split(""),
+          n = a.length;
+
+        for (var i = n - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var tmp = a[i];
+          a[i] = a[j];
+          a[j] = tmp;
+        }
+        return a.join("");
+      },
     };
   }
   // ---------------------------------------------------   Functions    -------------------------------------------------------------
