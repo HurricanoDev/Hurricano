92a840da9aab13f6659c5e783d5e1c8d455ad52a
Militia21 <70501605+Militia21@users.noreply.github.com>
2021-07-09 11:47:06 +0000

    kick/emoji fix.

--- bot/commands/moderation/addemoji.js
+++ bot/commands/moderation/addemoji.js
@@ -3,24 +3,35 @@ const { Util, MessageEmbed } = require("discord.js");
 const { parse } = require("twemoji-parser");
 module.exports = new Command({
   name: "addemoji",
-  description: "lets the user add a custom emoji through an image URL. (which should be less than 256 KB)",
+  description:
+    "lets the user add a custom emoji through an image URL. (which should be less than 256 KB)",
   userPermissions: ["MANAGE_EMOJIS"],
   async run(message, args) {
     const emoji = args[0];
     const name = args.slice(1).join(" ");
     if (!emoji) {
-      return message.channel.send(
-        `Please Give Me A Emoji!`
+      return message.channel.sendError(
+        message,
+        "Error.",
+        `Please give me an emoji!`
       );
     }
     if (!name) {
-      return message.channel.send(`No emoji name specified`)
+      return message.channel.sendError(
+        message,
+        "Error.",
+        `No emoji name specified`
+      );
     }
     try {
       if (emoji.startsWith("https://cdn.discordapp.com")) {
         const fb = await message.guild.emojis.create(emoji, name || name);
 
-        return message.channel.send(fb + ` has been added as "${name}"`);
+        return message.channel.sendSuccess(
+          message,
+          "Emoji added.",
+          fb + ` has been added as "${name}"`
+        );
       }
 
       const customEmoji = Util.parseEmoji(emoji);
@@ -28,20 +39,30 @@ module.exports = new Command({
       if (customEmoji.id) {
         const link = `https://cdn.discordapp.com/emojis/${customEmoji.id}.${
           customEmoji.animated ? "gif" : "png"
-          }`;
+        }`;
 
-        await message.guild.emojis.create(
+        const emj = await message.guild.emojis.create(
           `${link}`,
           `${name || `${customEmoji.name}`}`
         );
-        return message.channel.send(`<:${customEmoji.name}:${customEmoji.id}> has been added as ${name}`);
+        return message.channel.sendSuccess(
+          message,
+          "Done!",
+          `${emj} has been added as ${name}`
+        );
       } else {
         const foundEmoji = parse(emoji, { assetType: "png" });
         if (!foundEmoji[1]) {
-          return message.channel.send("Please provide a valid emoji. I can't work with this and it should be under 256 KB");
+          return message.channel.sendError(
+            message,
+            "Error.",
+            "Please provide a valid emoji. I can't work with this and it should be under 256 KB"
+          );
         }
 
-        message.channel.send(
+        message.channel.sendError(
+          message,
+          "Error!",
           "Bruv this is a normal emoji what you can use anywhere"
         );
       }
@@ -51,7 +72,9 @@ module.exports = new Command({
           "DiscordAPIError: Maximum number of emojis reached (50). I can't work with this and it should be under 256 KB"
         )
       ) {
-        return message.channel.send(
+        return message.channel.sendError(
+          message,
+          "Error.",
           "Maximum emoji count reached for this Server!"
         );
       }
--- bot/commands/moderation/kick.js
+++ bot/commands/moderation/kick.js
@@ -2,33 +2,57 @@ const Command = require("@Command");
 const { MessageEmbed } = require("discord.js");
 module.exports = new Command({
   name: "kick",
-  description: "This command could be used by the server moderator to kick a user.",
+  description:
+    "This command could be used by the server moderator to kick a user.",
   userPermissions: ["KICK_MEMBERS"],
   async run(message, args) {
-    const mentionedMember = message.mentions.members.first();
-    let reason = args.slice(1).join(" ");
-    if(!reason) reason = "No reason given"
+    const mentionedMember = message.mentions.members.first() || message.guild.users.cache.get(args[0]);
+    let reason = args.slice(1).join(" ") || "No reason provided.";
     const kickEmbed = new MessageEmbed()
       .setTitle(`You were kicked from ${message.guild.name}`)
       .setDescription(`Reason: ${reason}`)
       .setColor("#ffff")
       .setTimestamp()
-      .setFooter(client.user.tag ,client.user.displayAvatarURL());
-    
-    if(!args[0]) return message.channel.send("You need to state a user to kick. \`.kick @user reason\`");
-    if(!mentionedMember) return message.channel.send("The member mentioned is not in the server.");
-    if(!mentionedMember.kickable) return message.channel.send('I cannot kick that member.');
-    try{
+      .setFooter(client.user.tag, client.user.displayAvatarURL());
+
+    if (!args[0])
+      return message.channel.sendError(
+        message,
+        "Error.",
+        `\`${message._usedPrefix}kick <@member> <reeason>\`\nMember in args is missing.`
+      );
+    if (!mentionedMember)
+      return message.channel.sendError(
+        message,
+        "Error.",
+        "The member mentioned is not in the server."
+      );
+    if (!mentionedMember.kickable)
+      return message.channel.sendError(
+        message,
+        "Error.",
+        "I cannot kick that member."
+      );
+    try {
       await mentionedMember.send({ embeds: [kickEmbed] });
     } catch (err) {
       console.log(`I was unable to message the member.`);
     }
-    
-    try{
-      await mentionedMember.kick()
-    }catch (err) {
+
+    try {
+      message.channel.sendSuccess(
+        message,
+        "Done!",
+        `Kicked **${mentionedMember.user.tag}** successfully!`
+      );
+      await mentionedMember.kick();
+    } catch (err) {
       console.log(err);
-      return message.channel.send("I was unable to kick member mentioned .");
+      return message.channel.sendError(
+        message,
+        "Error.",
+        "I was unable to kick member mentioned ."
+      );
     }
   },
 });
