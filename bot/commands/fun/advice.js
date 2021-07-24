5b0feb7e61b765cd300bf1488a294b284d813b58
Achal Jhawar <35405812+achaljhawar@users.noreply.github.com>
2021-07-24 18:49:38 +0530

    Advice Command added

---
+++ bot/commands/fun/advice.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const Command = require("@Command");
+module.exports = new Command({
+    name: "advice",
+    userPermissions: ["SEND_MESSAGES"],
+    cooldown: 20,
+    description: "gives a random advice.",
+    async run(message, args) {
+        fetch('https://api.adviceslip.com/advice')
+            .then(res => res.json())
+            .then(json => {
+                const embed = new MessageEmbed()
+                    .setColor('#403B3A')
+                    .setAuthor(
+                        'Advice Slip',
+                        'https://i.imgur.com/8pIvnmD.png',
+                        'https://adviceslip.com/'
+                    )
+                    .setDescription(json.slip.advice)
+                    .setTimestamp()
+                    .setFooter('Powered by adviceslip.com', '');
+                message.channel.send({ embeds : [embed]});
+                return;
+            })
+            .catch(err => {
+                message.reply('Failed to deliver advice :sob:');
+                return console.error(err);
+            });
+    },
+});
